88083e4d0d1b324fa573680aef519298754ab0ab
Jordan Blackerby <152322656+JordanRAHHHH@users.noreply.github.com>
2026-09-03 20:32:50 -0500

    Add: design-hashmap - submission-4

---
+++ Data Structures & Algorithms/design-hashmap/submission-4.py
@@ -0,0 +1,36 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.table = [[] for i in range (250)]
+
+    def put(self, key: int, value: int) -> None:
+        index = hash(key) % len(self.table)
+
+        for i in range(len(self.table[index])):
+            if self.table[index][i][0] == key:
+                self.table[index][i][1] = value
+                return
+        self.table[index].append([key, value])
+
+    def get(self, key: int) -> int:
+        index = hash(key) % len(self.table)
+
+        for i in range(len(self.table[index])):
+            if self.table[index][i][0] == key:
+                return self.table[index][i][1]
+        return -1
+
+    def remove(self, key: int) -> None:
+        index = hash(key) % len(self.table)
+
+        for i in range(len(self.table[index])):
+            if self.table[index][i][0] == key:
+                self.table[index].pop(i)
+                return
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
